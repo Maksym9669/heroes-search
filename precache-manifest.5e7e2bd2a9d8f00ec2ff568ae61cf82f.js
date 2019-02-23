f4a6399850e8bc0009082cdf9456cf2e59b1ac34
Maksym9669 <maksym.sychov14@gmail.com>
2019-02-22 20:34:42 -0500

    Updates

---
+++ precache-manifest.5e7e2bd2a9d8f00ec2ff568ae61cf82f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e447018922f29de59948",
+    "url": "/heroes-search/static/css/main.6b69f8c5.chunk.css"
+  },
+  {
+    "revision": "e447018922f29de59948",
+    "url": "/heroes-search/static/js/main.e4470189.chunk.js"
+  },
+  {
+    "revision": "08efac45576a827a3574",
+    "url": "/heroes-search/static/js/runtime~main.08efac45.js"
+  },
+  {
+    "revision": "5bb62acd97ad4a52be18",
+    "url": "/heroes-search/static/js/2.5bb62acd.chunk.js"
+  },
+  {
+    "revision": "26354297ee980854bcbf4874c782b97e",
+    "url": "/heroes-search/index.html"
+  }
+];
